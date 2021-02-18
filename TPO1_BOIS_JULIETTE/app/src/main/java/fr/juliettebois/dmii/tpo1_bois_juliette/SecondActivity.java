a2dd0afe51ff9ae4477e0e66e90a7f6381943856
Juliette <juliette.bois@infomaniak.com>
2021-02-18 10:48:11 +0100

    Add TP2

---
+++ TPO1_BOIS_JULIETTE/app/src/main/java/fr/juliettebois/dmii/tpo1_bois_juliette/SecondActivity.java
@@ -0,0 +1,42 @@
+package fr.juliettebois.dmii.tpo1_bois_juliette;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import fr.juliettebois.dmii.tpo1_bois_juliette.databinding.ActivitySecondBinding;
+
+public class SecondActivity extends AppCompatActivity {
+    // variables
+    private Integer counter;
+
+    private ActivitySecondBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySecondBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        initViews();
+    }
+
+    private void initViews() {
+        counter = 0;
+        binding.result.setText(""+counter);
+
+        // Intercept click on the compute button
+        binding.calculateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                double number1 =  Double.parseDouble(String.valueOf(binding.numberOne.getText()));
+                double number2 =  Double.parseDouble(String.valueOf(binding.numberTwo.getText()));
+                double calcResult = number1 + number2;
+                //Toast.makeText(SecondActivity.this, ,Toast.LENGTH_SHORT).show();
+                binding.result.setText(Double.toString(calcResult));
+            }
+        });
+    }
+}
